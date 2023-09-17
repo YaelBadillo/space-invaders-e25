643db49e7d4b761e1f95b70c0f52d09e3a576b7f
Yael Badillo <yael.badillos@gmail.com>
2023-09-16 22:32:09 -0600

    Refactor: remove player movement code from the player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,36 +8,6 @@ public class Player : MonoBehaviour
     [SerializeField] private float life;
     [SerializeField] private int lives;
     [SerializeField] private GameObject liveSprites;
-    [SerializeField] private float speed;
-    private PlayerActions playerActions;
-    private Rigidbody2D playerRigidbody;
-    private Vector2 moveInput;
-
-    void Awake()
-    {
-        playerActions = new PlayerActions();
-
-        playerRigidbody = GetComponent<Rigidbody2D>();
-        if (playerRigidbody is null)
-            Debug.LogWarning("Rigidbody2D is null");
-    }
-
-    private void OnEnable()
-    {
-        playerActions.Player_Map.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerActions.Player_Map.Disable();
-    }
-
-    void FixedUpdate()
-    {
-        moveInput = playerActions.Player_Map.Movement.ReadValue<Vector2>();
-        moveInput.y = 0f;
-        playerRigidbody.velocity = moveInput * speed;
-    }
 
     public void TakeDamage(float damage)
     {
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private PlayerActions playerActions;
+    private Rigidbody2D playerRigidbody;
+    private Vector2 moveInput;
+
+    void Awake()
+    {
+        playerActions = new PlayerActions();
+
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        if (playerRigidbody is null)
+            Debug.LogWarning("A Rigidbody2D is required");
+    }
+
+    private void OnEnable()
+    {
+        playerActions.Player_Map.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerActions.Player_Map.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = playerActions.Player_Map.Movement.ReadValue<Vector2>();
+        moveInput.y = 0f;
+        playerRigidbody.velocity = moveInput * speed;
+    }
+}
