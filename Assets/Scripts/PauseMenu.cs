ab41e2e2f900c58ac41b80275254ec2383450007
Yael Badillo <yael.badillos@gmail.com>
2023-09-24 00:21:05 -0600

    Feat: add pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OpenCloseMenu();
+        }
+    }
+
+    private void OpenCloseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+}
