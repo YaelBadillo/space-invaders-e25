d1633af50597cc414946ca66957909d13e82e79c
Yael Badillo <yael.badillos@gmail.com>
2023-12-08 07:28:40 -0600

    Feat: add obstacle sprites

--- Assets/Scripts/Obstacle/Obstacle.cs
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -8,11 +8,54 @@ public class Obstacle : Entity
     /// <summary>
     /// The endurance of the obstacle.
     /// </summary>
-    [SerializeField] private float endurance = 6f;
-
+    private float endurance = 10f;
     protected override float Existence
     {
         get { return endurance; }
         set { endurance = value; }
+    }
+
+    public float Endurance
+    {
+        get { return endurance; }
+        set { endurance = value; }
+    }
+
+    float totalEndurance = 0f;
+
+    GameObject obstacleSprite;
+
+    [SerializeField]
+    Sprite brownSprite;
+
+    [SerializeField]
+    Sprite yellowSprite;
+
+    [SerializeField]
+    Sprite greenSprite;
+
+    void Start()
+    {
+        obstacleSprite = transform.GetChild(0).gameObject;
+        totalEndurance = Endurance;
+    }
+
+    void Update()
+    {
+        float percentage = Existence / totalEndurance;
+        Debug.Log(percentage);
+        Debug.Log(endurance);
+        if (percentage <= 0.25f)
+        {
+            obstacleSprite.GetComponent<SpriteRenderer>().sprite = brownSprite;
+        }
+        else if (percentage <= 0.5f)
+        {
+            obstacleSprite.GetComponent<SpriteRenderer>().sprite = yellowSprite;
+        }
+        else if (percentage <= 0.75f)
+        {
+            obstacleSprite.GetComponent<SpriteRenderer>().sprite = greenSprite;
+        }
     }
 }
